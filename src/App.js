8aac66892408d090bdad0e3a0d4ee670ddbd6957
ronardj01 <ronardj01@gmail.com>
2021-08-31 10:05:00 +0200

    created Login page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Signup from "./pages/authentication/Signup";
+import Login from "./pages/authentication/Login";
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
           </Route>
 
           <Route path="/auth/signup" component={Signup} />
+
+          <Route path="/auth/login" component={Login} />
         </Switch>
       </div>
     </Router>
---
+++ src/pages/authentication/Login.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import AuthInput from "../../components/authentication/AuthInput";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("handleSubmit Working");
+  };
+  return (
+    <div className="login">
+      <h1>Welcome!</h1>
+      <form className="form-login" onSubmit={handleSubmit}>
+        <AuthInput
+          label="Email"
+          inputType="email"
+          inputId="email-input"
+          inputValue={email}
+          onChange={setEmail}
+        />
+        <AuthInput
+          label="Password"
+          inputType="password"
+          inputId="password-input"
+          inputValue={password}
+          onChange={setPassword}
+        />
+        <button type="submit" className="btn">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+}
