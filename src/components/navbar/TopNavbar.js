e71d524fd605b500ea1a1e961d778d1b65d29439
Ronar de Jesus <ronardj01@gmail.com>
2021-09-15 20:29:37 +0200

    updated topnavar

--- src/components/navbar/TopNavbar.js
+++ src/components/navbar/TopNavbar.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { HiOutlineMenu } from 'react-icons/hi';
 
 export const TopNavbar = () => {
+
   return (
     <nav className="bg-blacky">
       {/* add logo */}
 
       {/* add navbar  */}
-      <ul className="flex justify-end items-center w-10/12 gap-12 h-20 font-semibold text-cream">
-        <li>
-        <Link to="/login">Login</Link>
-        </li>
-        <li>
-        <Link to="/signup">Signup</Link>
-        </li>
-      </ul>
+
+      <div className="flex flex-col items-center min-h-full font-semibold text-cream justify-end">
+        <button className="text-3xl hover-nav md:hidden"><HiOutlineMenu /></button>
+        <ul className="flex flex-col md:flex-row">
+          <li className="hover-nav">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="hover-nav">
+            <Link to="/login">Login</Link>
+          </li>
+          <li className="hover-nav">
+            <Link to="/signup">Signup</Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   )
 }
