0ecab739bf139ac741cc83b7797f7c79e398ec04
ronardj01 <ronardj01@gmail.com>
2021-11-30 10:16:00 +0100

    agregado clase md:text-2xl to home paragraph

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -5,7 +5,7 @@ export const Home = () => {
         <div>
             <h1 className="text-center my-4 text-3xl md:text-8xl font-title text-yellow-700">World Cooking</h1>
 
-            <p className="text-2xl text-center mx-20 font-intro">
+            <p className="md:text-2xl text-center mx-20 font-intro">
                 Enciende los fuegos y ponte el delantal, te ayudaremos a hacer las mejores
                 recetas de todo el mundo. Aquí podrás encontrar increíbles recetas con su paso
                 a paso y fantásticos consejos para que todo te salga delicioso.
